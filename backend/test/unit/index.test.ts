3299a7d91d443fdad3374c6f037fbe1affb0096b
leandro <ljpcarvalho@gmail.com>
2024-11-23 11:35:51 -0300

    chore: setup initial project structure for front and back

---
+++ backend/test/unit/index.test.ts
@@ -0,0 +1,10 @@
+import * as chai from 'chai';
+
+
+const { expect } = chai;
+
+describe('Test', () => {
+    it('should return true', () => {
+        expect(true).to.be.true;
+    });
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1>NovoProjeto</h1>
+    </>
+  )
+}
+
+export default App
