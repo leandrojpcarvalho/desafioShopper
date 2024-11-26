f1f5059d5424c2ce4450b8c9ab84eec6a3c8592f
leandro <ljpcarvalho@gmail.com>
2024-11-26 11:11:56 -0300

    reafactor: update sql queries to improve database

--- backend/src/db/SQL/create-database.sql
+++ backend/src/db/SQL/create-database.sql
@@ -8,7 +8,6 @@ CREATE TABLE IF NOT EXISTS drivers (
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     vehicle VARCHAR(255) NOT NULL,
-    rating VARCHAR(255) NOT NULL,
     tax INTEGER NOT NULL,
     min_order INTEGER NOT NULL,
     PRIMARY KEY (id)
@@ -20,10 +19,24 @@ CREATE TABLE IF NOT EXISTS rides (
     customer_id INT NOT NULL,
     origin VARCHAR(255) NOT NULL,
     destination VARCHAR(255) NOT NULL,
-    duration TIME NOT NULL,
+    duration VARCHAR(50) NOT NULL,
+    distance DECIMAL NOT NULL,
     value DECIMAL NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (driver_id) REFERENCES drivers (id) ON DELETE CASCADE,
     FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE
-);
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id INT NOT NULL AUTO_INCREMENT,
+    ride_id INT NOT NULL,
+    driver_id INT NOT NULL,
+    customer_id INT NOT NULL,
+    rating INTEGER NOT NULL,
+    comment VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    Foreign Key (customer_id) REFERENCES customers (id) ON DELETE CASCADE,
+    FOREIGN KEY (driver_id) REFERENCES drivers (id) ON DELETE CASCADE,
+    Foreign Key (ride_id) REFERENCES rides (id) ON DELETE CASCADE
+) ENGINE = InnoDB;
--- backend/src/db/SQL/seeds.sql
+++ backend/src/db/SQL/seeds.sql
@@ -2,35 +2,115 @@
 INSERT INTO `customers` (`id`) VALUES (NULL), (NULL);
 
 INSERT INTO
-    `drivers` (
-        `name`,
-        `vehicle`,
-        `rating`,
-        `tax`,
-        `min_order`,
-        `description`
+    drivers (
+        id,
+        name,
+        description,
+        vehicle,
+        tax,
+        min_order
     )
 VALUES (
-        'Homer Simpson',
-        'Plymouth Valiant 1973 rosa e enferrujado',
-        2.0,
-        2.50,
         1,
-        'Olá! Sou o Homer, seu motorista camarada! Relaxe e aproveite o passeio, com direito a rosquinhas e boas risadas (e talvez alguns desvios).'
+        'Homer Simpson',
+        'Olá! Sou o Homer, seu motorista camarada! Relaxe e aproveite o passeio, com direito a rosquinhas e boas risadas (e talvez alguns desvios).',
+        'Plymouth Valiant 1973 rosa e enferrujado',
+        2.50,
+        1
     ),
     (
+        2,
         'Dominic Toretto',
+        'Ei, aqui é o Dom. Pode entrar, vou te levar com segurança e rapidez ao seu destino. Só não mexa no rádio, a playlist é sagrada.',
         'Dodge Charger R/T 1970 modificado',
-        4.0,
         5.00,
-        5,
-        'Ei, aqui é o Dom. Pode entrar, vou te levar com segurança e rapidez ao seu destino. Só não mexa no rádio, a playlist é sagrada.'
+        5
     ),
     (
+        3,
         'James Bond',
+        'Boa noite, sou James Bond. À seu dispor para um passeio suave e discreto. Aperte o cinto e aproveite a viagem.',
         'Aston Martin DB5 clássico',
-        5.0,
         10.00,
-        10,
-        'Boa noite, sou James Bond. À seu dispor para um passeio suave e discreto. Aperte o cinto e aproveite a viagem.'
+        10
+    );
+
+INSERT INTO
+    rides (
+        id,
+        driver_id,
+        customer_id,
+        origin,
+        destination,
+        duration,
+        distance,
+        value,
+        date
+    )
+VALUES (
+        1,
+        1,
+        1,
+        'Springfield',
+        'Central Park',
+        '00:30:00',
+        10.0,
+        25.00,
+        CURRENT_TIMESTAMP
+    ),
+    (
+        2,
+        2,
+        2,
+        'Los Angeles',
+        'Toretto\'s Garage',
+        '00:20:00',
+        15.0,
+        75.00,
+        CURRENT_TIMESTAMP
+    ),
+    (
+        3,
+        3,
+        1,
+        'London',
+        'MI6 Headquarters',
+        '00:15:00',
+        5.0,
+        50.00,
+        CURRENT_TIMESTAMP
+    );
+
+INSERT INTO
+    reviews (
+        id,
+        ride_id,
+        driver_id,
+        customer_id,
+        rating,
+        comment
+    )
+VALUES (
+        1,
+        1,
+        1,
+        1,
+        2,
+        'Motorista simpático, mas errou o caminho 3 vezes. O carro cheira a donuts.'
+    ),
+    (
+        2,
+        2,
+        2,
+        2,
+        4,
+        'Que viagem incrível! O carro é um show à parte e o motorista, apesar de ter uma cara de poucos amigos, foi super gente boa. Recomendo!'
+    ),
+    (
+        3,
+        3,
+        3,
+        1,
+        5,
+        'Serviço impecável! O motorista é a própria definição de classe e o carro é simplesmente magnífico. Uma experiência digna de um agente secreto.'
     );
