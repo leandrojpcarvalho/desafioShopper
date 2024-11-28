11948ece0e93db5c5195a23f33d026b9d104f72c
leandro <ljpcarvalho@gmail.com>
2024-11-27 23:09:16 -0300

    chore: configuration coverage tests and env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 80,
+  },
+  define: {
+    'import.meta.env.VITE_GOOGLE_API_KEY': {
+      type: 'string',
+      value: process.env.GOOGLE_API_KEY
+    },
   }
 })
